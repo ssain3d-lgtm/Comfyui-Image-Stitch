1568cbd089e5310985c2bb6c3641c7187850a938
ssain3d-lgtm <244192902+ssain3d-lgtm@users.noreply.github.com>
2026-09-06 15:59:50 +0900

    feat: initialize Multi Stitch Images custom node

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .multi_stitch import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+WEB_DIRECTORY = "./web"
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    "WEB_DIRECTORY",
+]
